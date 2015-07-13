fb742d2e99b5042b1412899b94dd6a36d1fbf2f8
Andrzej Węgrzyn <andrzej9393@gmail.com>
2015-07-13 22:52:15 +0200

    Create Servlet UploadServlet

---
+++ src/main/java/UploadServlet.java
@@ -0,0 +1,46 @@
+
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ * Servlet implementation class UploadServlet
+ */
+@WebServlet("/UploadServlet")
+public class UploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public UploadServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String description = request.getParameter("description"); // Retrieves <input type="text" name="description">
+	    Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
+	    String fileName = getFileName(filePart);
+	    InputStream fileContent = filePart.getInputStream();	
+	    response.getWriter().println("OK");
+
+	}
+private static String getFileName(Part part) {
+    for (String cd : part.getHeader("content-disposition").split(";")) {
+        if (cd.trim().startsWith("filename")) {
+            String fileName = cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
+            return fileName.substring(fileName.lastIndexOf('/') + 1).substring(fileName.lastIndexOf('\\') + 1); // MSIE fix.
+        }
+    }
+    return null;
+}
+}
