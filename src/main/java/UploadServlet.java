7c8194eb1f1a263dc7e31e61a9b00a39d5ae1ca6
Andrzej Węgrzyn <andrzej9393@gmail.com>
2015-07-17 23:34:02 +0200

    multipartConfig

--- src/main/java/UploadServlet.java
+++ src/main/java/UploadServlet.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.util.Iterator;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +18,7 @@ import org.springframework.web.multipart.*;
 /**
  * Servlet implementation class UploadServlet
  */
+@MultipartConfig
 @WebServlet("/UploadServlet")
 public class UploadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
