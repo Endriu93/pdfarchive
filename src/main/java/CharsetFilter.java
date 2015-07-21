90344cb70e12060257acb7bf2833142b957f0712
Andrzej Węgrzyn <andrzej9393@gmail.com>
2015-07-21 22:55:08 +0200

    nmmmmm

--- src/main/java/CharsetFilter.java
+++ src/main/java/CharsetFilter.java
@@ -26,7 +26,7 @@ import javax.servlet.annotation.WebFilter;
    {
     // Respect the client-specified character encoding
     // (see HTTP specification section 3.4.1)
-    if(null == request.getCharacterEncoding())
+    //if(null == request.getCharacterEncoding())
       request.setCharacterEncoding(encoding);
 
 
