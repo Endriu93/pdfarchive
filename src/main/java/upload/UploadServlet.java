f0f7f455a5371bb84355649baaa78e5bde642006
Andrzej <Andrzej9393@gmail.com>
2015-08-17 12:31:27 +0200

    FileUpload

---
+++ src/main/java/upload/UploadServlet.java
@@ -0,0 +1,80 @@
+package upload;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.sql.SQLException;
+import java.util.Iterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
+
+import util.ReusableInputStream;
+import Core.Database;
+import Core.PDFManager;
+
+/**
+ * Servlet implementation class UploadServlet
+ */
+@MultipartConfig
+@WebServlet("/UploadServlet")
+public class UploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    response.setCharacterEncoding("UTF-8");
+	    response.setHeader("Content-Type","text/html;charset=UTF-8");
+	    
+	    
+	    Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
+	    String fileName = getFileName(filePart);
+	    InputStream fileContent = filePart.getInputStream();
+	   
+	    response.getWriter().println(fileName);
+	   
+	    
+	    Database database = new Database("pdfarchive","localhost" , "3306", "root", "pilot93");
+		PDFManager manager = new PDFManager(database);
+		
+		InputStream input = new ReusableInputStream(fileContent);
+		System.out.println("input available: "+input.available());
+		try {
+			manager.upload(input, "zasady przyjęcia do akademików", new String[]{"PK","Akademik"}, "Mieszkanie", false);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	// zwraca nazwę pliku przekazanego Partu
+	private static String getFileName(Part part) {
+		for (String cd : part.getHeader("Content-Disposition").split(";")) {
+			if (cd.trim().startsWith("filename")) {
+				String fileName = cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
+            return fileName.substring(fileName.lastIndexOf('/') + 1).substring(fileName.lastIndexOf('\\') + 1); // MSIE fix.
+			}
+		}
+    return null;
+	}
+	
+	    
+	
+}
