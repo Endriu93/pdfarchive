7d3e814ca3aa0b7b9e7cb10d6530c5101da3aade
Andrzej Węgrzyn <andrzej9393@gmail.com>
2015-08-31 00:12:51 +0200

    SimpleCORSFilter

---
+++ src/main/java/upload/SimpleCORSFilter2.java
@@ -0,0 +1,62 @@
+package upload;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class SimpleCORSFilter2
+ */
+@WebFilter("/SimpleCORSFilter2")
+public class SimpleCORSFilter2 implements Filter {
+
+	ServletContext context = null;
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		context = fConfig.getServletContext();
+	}
+    /**
+     * Default constructor. 
+     */
+    public SimpleCORSFilter2() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        
+        context.log("SimpleCorsFilter: doFilter()");
+		chain.doFilter(request, response);
+	}
+
+	
+
+}
