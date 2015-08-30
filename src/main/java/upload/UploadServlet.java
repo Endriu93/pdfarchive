51ca8540439fc98ffd5a64270586377050d09676
Andrzej Węgrzyn <andrzej9393@gmail.com>
2015-08-31 00:33:30 +0200

    cdcdcd

--- src/main/java/upload/UploadServlet.java
+++ src/main/java/upload/UploadServlet.java
@@ -34,6 +34,7 @@ public class UploadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("OK ");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
