61091bc0ac6f7cca945d6cad6e5cf93ebc833193
root <root@debian>
2016-01-10 21:05:34 +0100

    Database update. tested on localhost

---
+++ src/main/java/util/User.java
@@ -0,0 +1,21 @@
+package util;
+
+import java.io.StringReader;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.servlet.http.HttpServletRequest;
+
+public class User {
+
+	public static String getUserID(HttpServletRequest request) {
+		JsonReader reader = Json.createReader(new StringReader(request.getParameter("json"))); 
+		JsonObject json = reader.readObject();
+		
+		String userID = json.getString("userID");	
+		return userID;
+	}
+
+	
+}
--- src/main/resources/DatabaseDefinition.sql
+++ src/main/resources/DatabaseDefinition.sql
@@ -1,20 +1,12 @@
-create table if not exists Documents (
-DOCUMENT_ID int auto_increment not null primary key,
-DATA LONGBLOB not null,	-- raw Data
-AUTHOR_ID int not null references Authors(AUTHOR_ID),
-TITLE_ID text not null references Titles(TITLE_ID),
-DESCRIPTION text not null,
-ADD_DATE date not null,
-CREATE_DATE date default null, #--some PDFs hasn't this entry
-SIZE int not null check (SIZE>0) );#	-- number of pages
-
 #--drop index Documents_ADD_DATE on Documents;
 #--create index  Documents_ADD_DATE on Documents (ADD_DATE);
 
+
 create table if not exists Authors (
-AUTHOR_ID int auto_increment primary key not null,
+AUTHOR_ID int auto_increment not null,
 NAME text not null,
-unique key (NAME(25)));
+PRIMARY KEY(AUTHOR_ID),
+unique key(NAME(25)));
 
 #--drop index Authors_AUTHOR_NAME on Authors; 
 #--create index  Authors_AUTHOR_NAME on Authors(NAME(10));
@@ -28,19 +20,34 @@ unique key (NAME(25)));
 #--create index  Titles_NAME on Titles(NAME(10));
 
 create table if not exists Words (
-WORD_ID int auto_increment not null primary key ,
+WORD_ID int auto_increment not null primary key,
 NAME text not null,
 unique key (NAME(25)));
 
 #--drop index Words_Name on Words;
 #--create index  Words_NAME on Words(NAME(10));
 
+create table if not exists Documents (
+DOCUMENT_ID int auto_increment not null primary key,
+DATA LONGBLOB not null,	-- raw Data
+AUTHOR_ID int not null,
+TITLE_ID int not null,
+DESCRIPTION text not null,
+ADD_DATE date not null,
+CREATE_DATE date default null, #--some PDFs hasn't this entry
+SIZE int not null check (SIZE>0),
+FOREIGN KEY(AUTHOR_ID) references Authors(AUTHOR_ID),
+FOREIGN KEY(TITLE_ID) references Titles(TITLE_ID));#	-- number of pages
+
 create table if not exists DocumentWord (
-DOCUMENT_ID int not null references Documents(DOCUMENT_ID) ON DELETE CASCADE,
-WORD_ID int not null references Words(WORD_ID) ON DELETE CASCADE );
+DOCUMENT_ID int not null,
+WORD_ID int not null,
+FOREIGN KEY(DOCUMENT_ID) references Documents(DOCUMENT_ID) ON DELETE CASCADE,
+FOREIGN KEY(WORD_ID) references Words(WORD_ID) ON DELETE CASCADE);
 
 create table if not exists IndexInfo (
-DOCUMENT_ID int not null references Documents(DOCUMENT_ID) ON DELETE CASCADE,
+DOCUMENT_ID int not null,
+FOREIGN KEY(DOCUMENT_ID) references Documents(DOCUMENT_ID) ON DELETE CASCADE,
 IS_INDEXED Bool not null #-- if is indexed to words table
 );
 
@@ -55,20 +62,25 @@ unique key (NAME(25)));
 #--create index  Tags_NAME on Tags(NAME(10));
 
 create table if not exists DocumentTag (
-DOCUMENT_ID int not null references Documents(DOCUMENT_ID) ON DELETE CASCADE,
-TAG_ID int not null references Tags(TAG_ID) ON DELETE CASCADE );
+DOCUMENT_ID int not null,
+TAG_ID int not null,
+FOREIGN KEY(DOCUMENT_ID) references Documents(DOCUMENT_ID) ON DELETE CASCADE,
+FOREIGN KEY(TAG_ID) references Tags(TAG_ID) ON DELETE CASCADE);
 
 create table if not exists Categories (
 CATEGORY_ID int not null auto_increment primary key,
 NAME text not null,
 unique key (NAME(25)));
 
+insert into Categories values(1,'Rozne');
 #--drop index Category_NAME on Categories;
 #--create index  Category_NAME on Categories(NAME(10));
 
 create table if not exists DocumentCategory (
-DOCUMENT_ID int not null references Documents(DOCUMENT_ID) ON DELETE CASCADE,
-CATEGORY_ID int not null references Categories(CATEGORY_ID) ON DELETE CASCADE );
+DOCUMENT_ID int not null,
+CATEGORY_ID int not null,
+FOREIGN KEY(DOCUMENT_ID) references Documents(DOCUMENT_ID) ON DELETE CASCADE,
+FOREIGN KEY(CATEGORY_ID) references Categories(CATEGORY_ID) ON DELETE CASCADE);
 
 create table if not exists Users (
 USER_ID int primary key not null, #-- it will be used as sessionid hence this number must be complicated
@@ -77,5 +89,16 @@ PASSWORD text not null,
 unique key (LOGIN(30)));	#-- force in code to have login greater than 30 characters.
 
 create table if not exists DocumentUser (
-DOCUMENT_ID int not null references Documents(DOCUMENT_ID) ON DELETE CASCADE,
-USER_ID int not null references Users(USER_ID) ON DELETE CASCADE );
+DOCUMENT_ID int not null,
+USER_ID int not null,
+FOREIGN KEY(DOCUMENT_ID) references Documents(DOCUMENT_ID) ON DELETE CASCADE,
+FOREIGN KEY(USER_ID) references Users(USER_ID) ON DELETE CASCADE);
+
+create table if not exists CategoryUser (
+CATEGORY_ID int not null,
+USER_ID int not null,
+FOREIGN KEY(CATEGORY_ID) references Categories(CATEGORY_ID) ON DELETE CASCADE,
+FOREIGN KEY(USER_ID) references Users(USER_ID) ON DELETE CASCADE);
+
+CREATE TRIGGER onUserCreate after INSERT on Users 
+	FOR EACH ROW insert into CategoryUser values((select CATEGORY_ID from Categories where NAME='Rozne'),NEW.USER_ID);
